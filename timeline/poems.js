c8d99d659c578bfb46c269d46ca58c22d4f343bd
Cristyan Cadena <cristyan.cadena@gmail.com>
2019-06-26 17:32:54 -0400

    timeline WIP

---
+++ timeline/poems.js
@@ -0,0 +1,16 @@
+{
+    'poems': [
+         {
+              'date': 'May 5 1856',
+              'id': 3,
+              'title': 'A Utilitarian View of the Monitor\'s Fight'
+         },
+         {
+              'date': 'Oct 10 1855',
+              'id': 2,
+              'title': 'The Portent'
+         }
+    ],
+    'message': 'success',
+    'status': 200
+}
