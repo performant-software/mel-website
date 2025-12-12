1e8f9a53a3a012e6bfc498b682bb3e56e8c121dc
Rebecca Black <rebecca@performantsoftware.com>
2025-12-11 22:37:54 -0500

    adding EC artifacts build command

---
+++ scripts/ec-process.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+editioncrafter process -i  xml/hawthorne-and-his-mosses/mosses_tei.xml -o ec -u https://rb-mosses--mel.netlify.app/ec
+# editioncrafter process -i  xml/versions-of-billy-budd/bb-ms.xml -o ec -u https://rb-mosses--mel.netlify.app/ec
+
